eb12a0c1c0084c2bb80f7a6d51dc772675791838
IvanK <ivan_koyrushki@abv.bg>
2025-07-08 18:36:06 +0200

    pages fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/OnlineShop-Test/', // <-- This should match your repo name
   server: {
     port: 3001,
     open: true
   },
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     sourcemap: true
   }
 }) 
